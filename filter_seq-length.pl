60733182f68ba6ff96ac542d68943569a67d2359
Yuki Yoshioka <69947704+y-yoshioka1109@users.noreply.github.com>
2021-09-23 15:53:04 +0900

    Create filter_seq-length.pl

---
+++ filter_seq-length.pl
@@ -0,0 +1,28 @@
+#sort by sequence length and select longest one
+
+while(<>){
+  chomp;
+  @c=split(/\./,$_);
+  @a=split(/\t/,$_);
+  $id = $c[0];
+  $sco = $a[1]; ## here for seq length
+  if($id eq $pid){
+    $b{$sco} = $_;
+  }elsif($id ne $pid){
+    if (length ($pid)>1){
+      my @keys_sorted = sort  { $b <=> $a } keys %b; #sort
+      $top_id = shift @keys_sorted; # just get longest one 
+      print "$b{$top_id}\n";
+      %b=();
+    }
+  $b{$sco} = $_;
+  }
+$pid=$id;
+}
+
+if (length ($pid)>1){
+  my @keys_sorted = sort  { $b <=> $a } keys %b; #sort
+  $top_id = shift @keys_sorted; # just get longest one 
+  print "$b{$top_id}\n";
+  %b=();
+}
